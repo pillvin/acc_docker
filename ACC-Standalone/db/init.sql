489dc791719141912959494a2287451e08a9ef49
Vinod Pillai <pillvin@iit.edu>
2024-10-23 11:49:03 +0530

    postgres docker

---
+++ ACC-Standalone/db/init.sql
@@ -0,0 +1,7 @@
+-- create.ddl & master_data.sql are placed in
+-- /tmp mount point on postgres container.
+-- refer services.yaml volumes configuration
+
+
+\i /tmp/create.ddl
+\i /tmp/master_data.sql
